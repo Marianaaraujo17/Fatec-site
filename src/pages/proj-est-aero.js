b04a3b21627f5a7d28e1bf9b62d0545e5259f8cf
Brendo Bubela Vidal <brendobubela@gmail.com>
2022-03-19 01:18:05 -0300

    Criação de PEA e página Sobre os Cursos

--- src/pages/proj-est-aero.js
+++ src/pages/proj-est-aero.js
@@ -1,10 +1,131 @@
 import React from "react";
-
 import App from "../containers/App";
+import Footer from "../components/footer";
+import { Card, CardGroup, Container, Row, Col, Button } from "react-bootstrap";
+import "./proj-est-aero.css";
+
+const introducao = [
+  {
+    titulo: "Resumo",
+    descricao:
+      "O Tecnólogo em Projetos de Estruturas Aeronáuticas desenvolve projetos de componentes estruturais de uma aeronave, segundo as normas técnicas e de segurança. Projeta peças e estruturas, como as utilizadas nas asas, na empenagem (porção traseira do avião), na fuselagem e em outras partes da aeronave. Supervisiona a execução de projetos de fabricação de estruturas e peças. Pode atuar também na produção - industrial ou manual - de peças estruturais em compósitos (materiais compostos por dois ou mais componentes) e no gerenciamento de equipes na fábrica. O uso crescente de materiais leves na fabricação de estruturas aeronáuticas para diminuir o peso das aeronaves - o que melhora performances -, amplia o campo de trabalho para esse tecnólogo.",
+  },
+  {
+    titulo: "Onde este tecnólogo poderá trabalhar?",
+    descricao:
+      "Indústria aeronáutica, como montadoras de aviões e fabricantes de peças e estruturas aeronáuticas e órgãos públicos, como a Agência Nacional de Aviação Civil. O tecnólogo pode ainda abrir seu próprio negócio, por exemplo, uma empresa fornecedora de peças para as montadoras. Essa graduação tecnológica forma projetistas para trabalhar na Embraer por meio do Programa Projetista Embraer (PPE), uma parceria entre a empresa e a Fatec São José dos Campos.",
+  },
+];
+
+const links = [
+  {
+    titulo: "Inscreva-se",
+    subtitulo: "40 Vagas > Período da Noite > Duração de 06 semestres",
+    link: "https://www.vestibularfatec.com.br/home/",
+    botao: " www.vestibularfatec.com.br",
+  },
+  {
+    titulo: "Matriz curricular",
+    subtitulo: "",
+    link: "https://fatecsjc-prd.azurewebsites.net/downloads/cursos/pea/2016.11.16_matriz_pea.pdf",
+    botao: "Clique aqui para visualizar a matriz curricular e a carga horária",
+  },
+  {
+    titulo: "Projeto Pedagógico",
+    subtitulo: "",
+    link: "https://fatecsjc-prd.azurewebsites.net/downloads/cursos/pea/projeto_pedagogico_pea.pdf",
+    botao: "Clique aqui para visualizar o projeto pedagógico do curso.",
+  },
+  {
+    titulo: "Regulamentos",
+    subtitulo: "",
+    link: "https://fatecsjc-prd.azurewebsites.net/downloads/secretaria/regulamentogeral.pdf",
+    botao: "Regulamento Geral dos Cursos de Graduação das Fatecs",
+  },
+  {
+    titulo: "Docentes",
+    subtitulo: "",
+    link: "",
+    botao: "Clique aqui para visualizar a lista de docentes.",
+  },
+];
 
 const proj_est_aero = () => (
   <App>
-    <h1>Projetos de Estruturas Aeronáuticas</h1>
+    <Container
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+        marginBottom: "50px",
+      }}
+    >
+      <Row>
+        <Col>
+          <p className="titulo">PROJETOS DE ESTRUTURAS AERONÁUTICAS</p>
+          <p className="subtitulo">
+            40 Vagas no período da Noite | Controle e Processos Industriais
+          </p>
+        </Col>
+      </Row>
+    </Container>
+
+    <Container id="resumo" style={{ marginBottom: "50px" }}>
+      <Row>
+        <Col md={{ span: 12 }}>
+          <CardGroup>
+            {introducao.map((object, index) => {
+              return (
+                <Card style={{ marginRight: "50px" }}>
+                  <Card.Body>
+                    <Card.Title className="card-titulo">
+                      {object.titulo}
+                    </Card.Title>
+                    <Card.Text
+                      style={{ fontFamily: "Noto Sans", fontSize: "23px" }}
+                    >
+                      {object.descricao}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              );
+            })}
+          </CardGroup>
+        </Col>
+      </Row>
+    </Container>
+    <Container style={{ marginBottom: "50px", textAlign: "center" }}>
+      <Row xs={1} md={2} className="g-4">
+        {links.map((object, index) => {
+          return (
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Title className="card-link-titulo">
+                    {object.titulo}
+                  </Card.Title>
+                  <Card.Text className="card-link-subtitulo">
+                    {object.subtitulo}
+                  </Card.Text>
+                  <Button
+                    className="card-botao"
+                    variant="outline-success"
+                    href={object.link}
+                    target="_blank"
+                  >
+                    {object.botao}
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+
+    <Footer />
   </App>
 );
 
--- src/pages/sobre-cursos.js
+++ src/pages/sobre-cursos.js
@@ -1,10 +1,175 @@
 import React from "react";
-
 import App from "../containers/App";
+import Footer from "../components/footer";
+import {
+  Card,
+  CardGroup,
+  Container,
+  Row,
+  Col,
+  Button,
+  ListGroup,
+} from "react-bootstrap";
+import "./sobre-cursos.css";
 
 const sobre_cursos = () => (
   <App>
-    <h1>Sobre os Cursos</h1>
+    <Container
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+        marginBottom: "50px",
+      }}
+    >
+      <Row>
+        <Col>
+          <p className="titulo">CURSOS SUPERIORES GRATUITOS DE QUALIDADE!</p>
+        </Col>
+      </Row>
+    </Container>
+
+    <Container
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+        marginBottom: "50px",
+      }}
+    >
+      <Row xs={1} md={12} className="g-4">
+        <Col>
+          <p className="list-titulo">Cursos oferecidos nesta Unidade</p>
+          <ListGroup>
+            <ListGroup.Item>
+              <a className="links" href="/ads">
+                Análise e Desenvolvimento de Sistemas
+              </a>
+            </ListGroup.Item>
+            <ListGroup.Item variant="dark">
+              <a className="links" href="banco">
+                Banco de Dados
+              </a>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <a className="links" href="/dsm">
+                Desenvolvimento de Software Multiplataforma
+              </a>
+            </ListGroup.Item>
+            <ListGroup.Item variant="dark">
+              <a className="links" href="/gtproducaoind">
+                Gestão da Produção Industrial
+              </a>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <a className="links" href="/logistica">
+                Logística
+              </a>
+            </ListGroup.Item>
+            <ListGroup.Item variant="dark">
+              <a className="links" href="/manufatura">
+                Manufatura Avançada
+              </a>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <a className="links" href="/manut_aero">
+                Manutenção de Aeronaves
+              </a>
+            </ListGroup.Item>
+            <ListGroup.Item variant="dark">
+              <a className="links" href="/projtestaero">
+                Projetos de Estruturas Aeronáuticas
+              </a>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
+
+    <Container id="resumo" style={{ marginBottom: "50px" }}>
+      <Row>
+        <Col md={{ span: 12 }}>
+          <CardGroup>
+            <Card style={{ textAlign: "right" }}>
+              <Card.Body>
+                <Card.Title className="card-titulo">Inscrições</Card.Title>
+                <Card.Text>
+                  <a
+                    href="https://www.vestibularfatec.com.br/home/"
+                    target="_blank"
+                    className="inscricoes"
+                  >
+                    www.vestibularfatec.com.br
+                  </a>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Card>
+              <Card.Body>
+                <Card.Title className="card-titulo">
+                  Guia das profissões tecnológicas
+                </Card.Title>
+                <Card.Text
+                  style={{ fontFamily: "Noto Sans", fontSize: "23px" }}
+                >
+                  <p>
+                    Este{" "}
+                    <a
+                      className="guia"
+                      href="https://www.cps.sp.gov.br/download/guia_profissoes_tecnologicas.pdf"
+                      target="_blank"
+                    >
+                      GUIA
+                    </a>
+                    apresenta os cursos tecnológicos oferecidos pelo Centro
+                    Paula Souza, as disciplinas estudadas, as atribuições do
+                    profissional, onde trabalhar, onde estudar e sugestões de
+                    cursos relacionados. Boa sorte na escolha!
+                  </p>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </CardGroup>
+        </Col>
+        <Col md={{ span: 12 }}>
+          <CardGroup>
+            <Card>
+              <Card.Body>
+                <Card.Title className="card-titulo">
+                  Curso Superior de Tecnologia
+                </Card.Title>
+                <Card.Text
+                  style={{ fontFamily: "Noto Sans", fontSize: "23px" }}
+                >
+                  <p
+                    style={{
+                      fontWeight: "lighter",
+                      color: "gray",
+                      fontStyle: "italic",
+                    }}
+                  >
+                    ❝ É um curso de graduação (…). Desenvolve competências
+                    profissionais, fundamentadas na ciência, na tecnologia, na
+                    cultura e na ética (…). Como todo curso de nível superior, o
+                    curso dessa natureza é aberto a candidatos que tenham
+                    concluído o ensino médio, ou equivalente, e que tenham sido
+                    classificados em processo seletivo. Os graduados nos CST
+                    denominam-se tecnólogos e são profissionais de nível
+                    superior. (MEC, 2009) ❞
+                  </p>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </CardGroup>
+        </Col>
+      </Row>
+    </Container>
+
+    <Footer />
   </App>
 );
 
